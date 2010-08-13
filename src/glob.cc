0c61b8f3f69449988ed189d60e3cbcac71268175
isaacs <i@izs.me>
2010-08-12 21:52:34 -0700

    Don't expose a glob_t thing to JS.  It's just dumb to do that.

--- src/glob.cc
+++ src/glob.cc
@@ -45,138 +45,112 @@ Throw (Handle<String> msg) {
 }
 
 
-class Glob : public ObjectWrap {
-  static Persistent<FunctionTemplate> ctor;
-public:
-  glob_t g;
-  Glob (glob_t gg) : g(gg) {}
-  virtual ~Glob () {
-    globfree(&g);
-  }
-  static void Initialize (Handle<Object> target) {
-    HandleScope scope;
-    Local<FunctionTemplate> t = FunctionTemplate::New(New);
-    ctor = Persistent<FunctionTemplate>::New(t);
-    ctor->InstanceTemplate()->SetInternalFieldCount(1);
-    ctor->SetClassName(String::NewSymbol("glob_t"));
-    target->Set(String::NewSymbol("glob_t"), ctor->GetFunction());
-  }
-  static Handle<Value> New(const Arguments &args) {
-    HandleScope scope;
-    glob_t gg;
-    Glob *self = new Glob(gg);
-    self->Wrap(args.This());
-    return scope.Close(args.This());
-  }
 
-  // async EIO globbing
-  struct glob_request {
-    Persistent<Function> cb;
-    Glob *GT;
-    int retval;
-    int flags;
-    char *pattern;
-  };
-  static int EIO_Glob (eio_req *req) {
-    glob_request *gr = (glob_request *)req->data;
-    gr->retval = glob(gr->pattern, gr->flags, NULL, &(gr->GT->g));
-    return 0;
-  }
-  static int EIO_GlobAfter (eio_req *req) {
-    HandleScope scope;
-    ev_unref(EV_DEFAULT_UC);
-    glob_request *gr = (glob_request *)req->data;
-    Glob *GT = gr->GT;
-    glob_t *g = &(GT->g);
+// async EIO globbing
+struct glob_request {
+  Persistent<Function> cb;
+  glob_t *g;
+  int retval;
+  int flags;
+  char *pattern;
+};
+static int EIO_Glob (eio_req *req) {
+  glob_request *gr = (glob_request *)req->data;
+  gr->retval = glob(gr->pattern, gr->flags, NULL, gr->g);
+  return 0;
+}
+static int EIO_GlobAfter (eio_req *req) {
+  HandleScope scope;
+  ev_unref(EV_DEFAULT_UC);
+  glob_request *gr = (glob_request *)req->data;
+  glob_t *g = gr->g;
 
-    Local<Value> argv[2];
-    if (gr->retval != 0) {
-      argv[0] = Exception::Error(GlobError(gr->retval));
-    } else {
-      Local<Array> pathv = Array::New(g->gl_pathc);
-      for (int i = 0; i < g->gl_pathc; i ++) {
-        pathv->Set(Integer::New(i), String::New(g->gl_pathv[i]));
-      }
-      argv[0] = Local<Value>::New(Null());
-      argv[1] = pathv;
-    }
-
-    TryCatch try_catch;
-    gr->cb->Call(Context::GetCurrent()->Global(), 2, argv);
-    if (try_catch.HasCaught()) {
-      FatalException(try_catch);
-    }
-    GT->Unref();
-    gr->cb.Dispose();
-    free(gr);
-    return 0;
-  }
-  static Handle<Value> GlobAsync (const Arguments& args) {
-    HandleScope scope;
-    const char *usage = "usage: glob(pattern, flags, globt, cb)";
-
-    if (args.Length() != 4) {
-      Throw(usage);
-    }
-
-    String::Utf8Value pattern(args[0]);
-    int flags = args[1]->Int32Value();
-    Glob *GT = ObjectWrap::Unwrap<Glob>(args[2]->ToObject());
-    Local<Function> cb = Local<Function>::Cast(args[3]);
-
-    glob_request *gr = (glob_request *)malloc(sizeof(glob_request));
-    gr->cb = Persistent<Function>::New(cb);
-    gr->pattern = *pattern;
-    gr->flags = flags;
-    gr->GT = GT;
-    GT->Ref();
-    eio_custom(EIO_Glob, EIO_PRI_DEFAULT, EIO_GlobAfter, gr);
-    ev_ref(EV_DEFAULT_UC);
-
-    return Undefined();
-  }
-  
-  // synchronous globbing.
-  static Handle<Value> GlobSync (const Arguments& args) {
-    HandleScope scope;
-    const char * usage = "usage: globSync(pattern, flags, globt)";
-    if (args.Length() != 3) {
-      return Throw(usage);
-    }
-
-    String::Utf8Value pattern(args[0]);
-
-    int flags = args[1]->Int32Value();
-
-    Glob *GT = ObjectWrap::Unwrap<Glob>(args[2]->ToObject());
-    glob_t *g = &(GT->g);
-    int retval = glob(*pattern, flags, NULL, g);
-
-    if (retval != 0) {
-      return Throw(retval);
-    }
-
-    // create a JS array
-    // loop through the g->gl_pathv adding JS strings to the JS array.
-    // then return the JS array.
-    Handle<Array> pathv = Array::New(g->gl_pathc);
+  Local<Value> argv[2];
+  if (gr->retval != 0) {
+    argv[0] = Exception::Error(GlobError(gr->retval));
+  } else {
+    Local<Array> pathv = Array::New(g->gl_pathc);
     for (int i = 0; i < g->gl_pathc; i ++) {
       pathv->Set(Integer::New(i), String::New(g->gl_pathv[i]));
     }
-
-    return pathv;
+    argv[0] = Local<Value>::New(Null());
+    argv[1] = pathv;
   }
-};
+
+  TryCatch try_catch;
+  gr->cb->Call(Context::GetCurrent()->Global(), 2, argv);
+  if (try_catch.HasCaught()) {
+    FatalException(try_catch);
+  }
+  // fprintf(stderr, "EIO_GlobAfter about to free\n");
+  gr->cb.Dispose();
+  globfree(g);
+  free(gr);
+  // fprintf(stderr, "EIO_GlobAfter freed\n");
+  return 0;
+}
+static Handle<Value> GlobAsync (const Arguments& args) {
+  HandleScope scope;
+  const char *usage = "usage: glob(pattern, flags, cb)";
+
+  if (args.Length() != 3) {
+    Throw(usage);
+  }
+
+  String::Utf8Value pattern(args[0]);
+  int flags = args[1]->Int32Value();
+  Local<Function> cb = Local<Function>::Cast(args[2]);
+
+  glob_request *gr = (glob_request *)malloc(sizeof(glob_request));
+  gr->cb = Persistent<Function>::New(cb);
+  gr->pattern = *pattern;
+  gr->flags = flags;
+  gr->g = new glob_t;
+  eio_custom(EIO_Glob, EIO_PRI_DEFAULT, EIO_GlobAfter, gr);
+  ev_ref(EV_DEFAULT_UC);
+
+  return Undefined();
+}
+
+// synchronous globbing.
+static Handle<Value> GlobSync (const Arguments& args) {
+  HandleScope scope;
+  const char * usage = "usage: globSync(pattern, flags)";
+  if (args.Length() != 2) {
+    return Throw(usage);
+  }
+
+  String::Utf8Value pattern(args[0]);
+
+  int flags = args[1]->Int32Value();
+
+  glob_t g;
+  int retval = glob(*pattern, flags, NULL, &g);
+
+  if (retval != 0) {
+    globfree(&g);
+    return Throw(retval);
+  }
+
+  // create a JS array
+  // loop through the g.gl_pathv adding JS strings to the JS array.
+  // then return the JS array.
+  Handle<Array> pathv = Array::New(g.gl_pathc);
+  for (int i = 0; i < g.gl_pathc; i ++) {
+    pathv->Set(Integer::New(i), String::New(g.gl_pathv[i]));
+  }
+
+  globfree(&g);
+  return scope.Close(pathv);
+}
 
 
-Persistent<FunctionTemplate> Glob::ctor;
 extern "C" void
 init (Handle<Object> target) 
 {
   HandleScope scope;
-  NODE_SET_METHOD(target, "glob", Glob::GlobAsync);
-  NODE_SET_METHOD(target, "globSync", Glob::GlobSync);
-  Glob::Initialize(target);
   GlobConstants(target);
+  NODE_SET_METHOD(target, "glob", GlobAsync);
+  NODE_SET_METHOD(target, "globSync", GlobSync);
 }
 
