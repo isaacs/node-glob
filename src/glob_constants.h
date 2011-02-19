1d6c1619cb0055f91853f5ffef870e3b71cbc11a
isaacs <i@izs.me>
2011-02-19 13:34:21 -0800

    Define consts better.
    Since we know what's defined and what isn't, there's no need
    for the ifdef stuff.

--- src/glob_constants.h
+++ src/glob_constants.h
@@ -9,100 +9,56 @@ using namespace v8;
 void
 GlobConstants (Handle<Object> target) {
   // flags
+  // These are defined in deps/fnmatch/fnmatch.h and deps/glob/glob.h
 
-// this is actually useless, because the glob_t object isn't exposed
+// these are actually useless, because the glob_t object isn't exposed
 // to the javascript layer.  Since it's so trivial to concatenate
 // arrays in JS anyway, it's a bit silly to even bother with this.
-// #ifdef GLOB_APPEND
-//   NODE_DEFINE_CONSTANT(target, GLOB_APPEND);
-// #endif
+// NODE_DEFINE_CONSTANT(target, GLOB_APPEND);
+// NODE_DEFINE_CONSTANT(target, GLOB_DOOFFS);
 
 // this is useless as well, because there is no way to supply the errfunc
 // function to glob().  It's just handled like the rest of node, passing
 // an Error() obj to the cb.
-// #ifdef GLOB_ERR
-//   NODE_DEFINE_CONSTANT(target, GLOB_ERR);
-// #endif
+// NODE_DEFINE_CONSTANT(target, GLOB_ERR);
 
-#ifdef GLOB_MARK
-  NODE_DEFINE_CONSTANT(target,GLOB_MARK);
-#endif
-#ifdef GLOB_NOCHECK
-  NODE_DEFINE_CONSTANT(target,GLOB_NOCHECK);
-#endif
-#ifdef GLOB_NOSORT
-  NODE_DEFINE_CONSTANT(target, GLOB_NOSORT);
-#endif
-#ifdef GLOB_NOESCAPE
-  NODE_DEFINE_CONSTANT(target, GLOB_NOESCAPE);
-#endif
+// Not supported, since the glob_t object isn't exposed directly.
+// NODE_DEFINE_CONSTANT(target, GLOB_ALTDIRFUNC);
 
-#ifdef GLOB_NOSPACE
-  NODE_DEFINE_CONSTANT(target, GLOB_NOSPACE);
-#endif
-#ifdef GLOB_ABORTED
-  NODE_DEFINE_CONSTANT(target, GLOB_ABORTED);
-#endif
-#ifdef GLOB_NOMATCH
-  NODE_DEFINE_CONSTANT(target, GLOB_NOMATCH);
-#endif
-#ifdef GLOB_NOSYS
-  NODE_DEFINE_CONSTANT(target, GLOB_NOSYS);
-#endif
+NODE_DEFINE_CONSTANT(target, GLOB_MARK);
+NODE_DEFINE_CONSTANT(target, GLOB_NOCHECK);
+NODE_DEFINE_CONSTANT(target, GLOB_NOSORT);
+NODE_DEFINE_CONSTANT(target, GLOB_NOESCAPE);
 
-#ifdef GLOB_ALTDIRFUNC
-  NODE_DEFINE_CONSTANT(target, GLOB_ALTDIRFUNC);
-#endif
-#ifdef GLOB_BRACE
-  NODE_DEFINE_CONSTANT(target, GLOB_BRACE);
-#endif
-#ifdef GLOB_MAGCHAR
-  NODE_DEFINE_CONSTANT(target, GLOB_MAGCHAR);
-#endif
-#ifdef GLOB_NOMAGIC
-  NODE_DEFINE_CONSTANT(target, GLOB_NOMAGIC);
-#endif
-#ifdef GLOB_LIMIT
-  NODE_DEFINE_CONSTANT(target, GLOB_LIMIT);
-#endif
-#ifdef GLOB_TILDE
-  NODE_DEFINE_CONSTANT(target, GLOB_TILDE);
-#endif
-#ifdef GLOB_PERIOD
-  NODE_DEFINE_CONSTANT(target, GLOB_PERIOD);
-#endif
-#ifdef GLOB_NO_DOTDIRS
-  NODE_DEFINE_CONSTANT(target, GLOB_NO_DOTDIRS);
-#endif
-#ifdef GLOB_STAR
-  NODE_DEFINE_CONSTANT(target, GLOB_STAR);
-#endif
-#ifdef GLOB_QUOTE
-  NODE_DEFINE_CONSTANT(target, GLOB_QUOTE);
-#endif
+NODE_DEFINE_CONSTANT(target, GLOB_NOSPACE)
+NODE_DEFINE_CONSTANT(target, GLOB_ABORTED);
+NODE_DEFINE_CONSTANT(target, GLOB_NOMATCH);
+NODE_DEFINE_CONSTANT(target, GLOB_NOSYS);
 
-#ifdef GLOB_ABEND
-  NODE_DEFINE_CONSTANT(target, GLOB_ABEND);
-#endif
+NODE_DEFINE_CONSTANT(target, GLOB_BRACE);
+NODE_DEFINE_CONSTANT(target, GLOB_MAGCHAR);
+NODE_DEFINE_CONSTANT(target, GLOB_NOMAGIC);
+NODE_DEFINE_CONSTANT(target, GLOB_LIMIT);
+NODE_DEFINE_CONSTANT(target, GLOB_TILDE);
+
+NODE_DEFINE_CONSTANT(target, GLOB_PERIOD);
+NODE_DEFINE_CONSTANT(target, GLOB_NO_DOTDIRS);
+NODE_DEFINE_CONSTANT(target, GLOB_STAR);
+NODE_DEFINE_CONSTANT(target, GLOB_QUOTE);
+NODE_DEFINE_CONSTANT(target, GLOB_ABEND);
 
 // the fnmatch stuff
-#ifdef FNM_NOESCAPE
-  NODE_DEFINE_CONSTANT(target, FNM_NOESCAPE);
-#endif
-#ifdef FNM_PATHNAME
-  NODE_DEFINE_CONSTANT(target, FNM_PATHNAME);
-#endif
-#ifdef FNM_PERIOD
-  NODE_DEFINE_CONSTANT(target, FNM_PERIOD);
+NODE_DEFINE_CONSTANT(target, FNM_NOMATCH);
+NODE_DEFINE_CONSTANT(target, FNM_NOSYS);
+NODE_DEFINE_CONSTANT(target, FNM_NORES);
+NODE_DEFINE_CONSTANT(target, FNM_NOESCAPE);
+NODE_DEFINE_CONSTANT(target, FNM_PATHNAME);
+NODE_DEFINE_CONSTANT(target, FNM_PERIOD);
+#ifdef FNM_CASEFOLD
+NODE_DEFINE_CONSTANT(target, FNM_CASEFOLD);
 #endif
 #ifdef FNM_LEADING_DIR
-  NODE_DEFINE_CONSTANT(target, FNM_LEADING_DIR);
-#endif
-#ifdef FNM_CASEFOLD
-  NODE_DEFINE_CONSTANT(target, FNM_CASEFOLD);
-#endif
-#ifdef FNM_NOMATCH
-  NODE_DEFINE_CONSTANT(target, FNM_NOMATCH);
+NODE_DEFINE_CONSTANT(target, FNM_LEADING_DIR);
 #endif
 
 }
