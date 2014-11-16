367f9ec2748d14779841a5e1ffda39dec8359628
isaacs <i@izs.me>
2014-11-16 15:47:52 -0800

    put benchmark script in TMPDIR instead of project
    Too tedious to continually switch back and forth between tests and
    profiling

--- make-benchmark-fixture.sh
+++ make-benchmark-fixture.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+
+tmp=${TMPDIR:-/tmp}
 export CDPATH=
 set -e
-if ! [ -d benchmark-fixture ]; then
+if ! [ -d $tmp/benchmark-fixture ]; then
   echo Making benchmark fixtures
-  mkdir benchmark-fixture
-  cd benchmark-fixture
+  mkdir $tmp/benchmark-fixture
+  cd $tmp/benchmark-fixture
   dirnames=`echo {0..9}/{0..9}/{0..9}/{0..9}` # 10000 dirs
   filenames=`echo {0..9}/{0..9}/{0..9}/{0..9}/{0..9}.txt`
   echo $dirnames | xargs mkdir -p
