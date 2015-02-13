0fcf9c966885a91673340dd707ea61efb935efea
Anton Backer <olegov@gmail.com>
2015-02-12 22:47:34 -0500

    Add UNC path unit test

--- test/zz-cleanup.js
+++ test/zz-cleanup.js
@@ -3,6 +3,19 @@ require("./global-leakage.js")
 var tap = require("tap")
 , rimraf = require("rimraf")
 , path = require("path")
+, spawn = require('child_process').spawn
+
+// remove unc share
+if (process.platform === 'win32') {
+  tap.test('remove unc-accessible share', function (t) {
+    var net = spawn('net', ['share', 'glob-test', '/y', '/delete'])
+    net.stderr.pipe(process.stderr)
+    net.on('close', function (code) {
+      t.equal(code, 0, 'failed to remove unc share')
+      t.end()
+    })
+  })
+}
 
 tap.test("cleanup fixtures", function (t) {
   rimraf(path.resolve(__dirname, "fixtures"), function (er) {
