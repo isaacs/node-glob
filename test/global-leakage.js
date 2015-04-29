fa29df5ae14c4eb00ae921d48713e33f853a1a5d
isaacs <i@izs.me>
2015-04-29 11:27:18 -0400

    Better global detection
    Newer versions of node-tap don't create a TAP_Global_Harness global

--- test/global-leakage.js
+++ test/global-leakage.js
@@ -1,11 +1,16 @@
 if (require.main === module)
   return console.log('ok')
 
-var before = Object.keys(global).concat('TAP_Global_Harness').sort().join(':')
+var before = Object.keys(global).sort().join(':')
 var assert = require('assert')
 var glob = require('../')
 
 process.on('exit', function() {
+  delete global.TAP_Global_Harness
   var after = Object.keys(global).sort().join(':')
+  if (after !== before) {
+    console.log('- ' + before)
+    console.log('+ ' + after)
+  }
   assert.equal(before, after)
 })
