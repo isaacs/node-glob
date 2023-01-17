0142c85947a355da91057913d9a96891fedb60d2
isaacs <i@izs.me>
2023-01-17 14:21:53 -0800

    update benchmark script
    Only show the real time, and include glob v7/v8 for comparison.

--- benchmark.sh
+++ benchmark.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 export CDPATH=
+set -e
 
 tmp=${TMPDIR:-/tmp}
 bash make-benchmark-fixture.sh
 wd=$PWD
 cd $tmp/benchmark-fixture
 
+tt () {
+  time "$@"
+}
+
+t () {
+  tt "$@" 2>&1 | grep real
+}
+
 set -e
 
-if [[ "`bash --version`" =~ version\ 4 ]]; then
+if [[ "`bash --version`" =~ version\ 4 ]] || [[ "`bash --version`" =~ version\ 5 ]]; then
   echo Bash timing:
-  time bash -c 'shopt -s globstar; echo **/*.txt | wc -w'
+  t bash -c 'shopt -s globstar; echo **/*.txt | wc -w' | grep real
 fi
 
 echo
-if type zsh; then
+if type zsh &>/dev/null; then
   echo Zsh timing:
-  time zsh -c 'echo **/*.txt | wc -w'
+  t zsh -c 'echo **/*.txt | wc -w'
 fi
 
 echo
 
 echo Node statSync and readdirSync timing:
-time node -e '
+t node -e '
   var fs=require("fs");
   var count = 0;
   function walk (path) {
@@ -35,23 +44,58 @@ time node -e '
     }
   }
   walk(".");
-  console.log(count)'
+  console.log(count)' | grep real
 echo
 
-echo Node glob.sync timing:
-time node -e '
+mkdir -p "$wd/old/7"
+echo '{"dependencies":{"glob":"7"}}' > "$wd/old/7/package.json"
+(cd "$wd/old/7" &>/dev/null; npm i --silent)
+
+echo Node glob v7 sync timing:
+t node -e '
+  var glob=require(process.argv[1])
+  console.log(glob.sync("**/*.txt").length)
+' "$wd/old/7/node_modules/glob" | grep real
+echo
+
+echo Node glob v7 async timing:
+t node -e '
+  var glob=require(process.argv[1])
+  glob("**/*.txt", (er, files) => {
+    console.log(files.length)
+  })' "$wd/old/7/node_modules/glob" | grep real
+echo
+
+mkdir -p "$wd/old/8"
+echo '{"dependencies":{"glob":"8"}}' > "$wd/old/8/package.json"
+(cd "$wd/old/8" &>/dev/null; npm i --silent)
+
+echo Node glob v8 sync timing:
+t node -e '
+  var glob=require(process.argv[1])
+  console.log(glob.sync("**/*.txt").length)
+' "$wd/old/8/node_modules/glob"
+echo
+
+echo Node glob v8 async timing:
+t node -e '
+  var glob=require(process.argv[1])
+  glob("**/*.txt", (er, files) => {
+    console.log(files.length)
+  })' "$wd/old/8/node_modules/glob"
+echo
+
+
+echo Node current glob.sync timing:
+t node -e '
   var glob=require(process.argv[1]);
   console.log(glob.sync("**/*.txt").length);' "$wd"
 echo
 
-echo Node glob async timing:
-time node -e '
+echo Node current glob async timing:
+t node -e '
   var glob=require(process.argv[1]);
-  glob("**/*.txt", function (er, files) {
+  glob("**/*.txt").then((files) => {
     console.log(files.length)
   })' "$wd"
 echo
-
-echo Node glob with --prof
-cd $wd
-bash prof.sh
--- prof.sh
+++ prof.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 export CDPATH=
 set -e
+set -x
 
 tmp=${TMPDIR:-/tmp}
-bash make-benchmark-fixture.sh
+bash -x make-benchmark-fixture.sh
 wd=$PWD
-cd $tmp/benchmark-fixture
+cd "$tmp/benchmark-fixture"
 
-node --prof -e '
-  var glob=require(process.argv[1]);
-  glob("**/*.txt", function (er, files) {
-    console.log(files.length)
-  })
-  //console.log(glob.sync("**/*.txt").length);
-  ' "$wd"
+cat > "profscript.mjs" <<MJS
+import glob from '$wd/dist/mjs/index.js'
+console.log(glob.sync("**/*.txt").length);
+// glob("**/*.txt").then((files) => {
+//   console.log(files.length)
+// })
+MJS
+
+node --prof "$tmp/benchmark-fixture/profscript.mjs"
 mv *v8.log "$wd/v8.log"
 cd "$wd"
 node-tick-processor > profile.txt
