58159ca6767e7f8925e9301b9c0ac11211bde50b
isaacs <i@izs.me>
2023-02-27 12:47:00 -0800

    test: cwd can be a url

---
+++ test/url-cwd.ts
@@ -0,0 +1,15 @@
+import t from 'tap'
+import {pathToFileURL} from 'url'
+import {Glob} from '../'
+
+t.test('can use file url as cwd option', t => {
+  const fileURL = pathToFileURL(process.cwd())
+  const fileURLString = String(fileURL)
+  const ps = new Glob('.', { cwd: process.cwd()})
+  const pu = new Glob('.', { cwd: fileURL })
+  const pus = new Glob('.', { cwd: fileURLString })
+  t.equal(ps.cwd, process.cwd())
+  t.equal(pu.cwd, process.cwd())
+  t.equal(pus.cwd, process.cwd())
+  t.end()
+})
