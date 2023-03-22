2054644b0682bc029616afd537718ea2aa765aac
isaacs <i@izs.me>
2023-03-21 17:56:02 -0700

    even more comprehensive tests for #515

--- test/memfs.ts
+++ test/memfs.ts
@@ -2,6 +2,7 @@ if (process.platform === 'win32') {
   t.plan(0, 'this test does not work on windows')
   process.exit(0)
 }
+
 import { fs as memfs, vol } from 'memfs'
 import t from 'tap'
 import { glob } from '../'
@@ -9,12 +10,28 @@ t.beforeEach(() => vol.fromJSON({ '/x': 'abc' }))
 
 const fs = memfs as unknown as typeof import('fs')
 
-t.test('should match single file with pattern', async t => {
-  const expandedFiles = await glob(['.', '/**/*'], { nodir: true, fs })
-  t.strictSame(expandedFiles, ['/x'])
-})
+const mock = {
+  fs: memfs,
+  'fs/promises': memfs.promises,
+}
 
-t.test('should match single file without pattern', async t => {
-  const expandedFiles = await glob(['.', '/x'], { nodir: true, fs })
-  t.strictSame(expandedFiles, ['/x'])
-})
+const patterns = ['/**/*', '/*', '/x']
+const cwds = ['/', undefined]
+for (const pattern of patterns) {
+  t.test(pattern, async t => {
+    for (const cwd of cwds) {
+      t.test(`cwd=${cwd}`, async t => {
+        t.test('mocking the fs', async t => {
+          const { glob } = t.mock('../', {
+            ...mock,
+            'path-scurry': t.mock('path-scurry', mock),
+          })
+          t.strictSame(await glob(pattern, { nodir: true, cwd }), ['/x'])
+        })
+        t.test('passing in fs argument', async t => {
+          t.strictSame(await glob(pattern, { nodir: true, cwd, fs }), ['/x'])
+        })
+      })
+    }
+  })
+}
